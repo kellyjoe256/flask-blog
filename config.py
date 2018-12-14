d84f1f71497e9af150e761e89f96952404aa3bfc
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2018-12-14 22:10:29 +0300

    Changed dev database uri

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class DevelopmentConfig(Config):
     ENV = 'development'
     DEBUG = True
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 
 
