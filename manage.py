cff739c47357d689475bb0310ea648085c825a5b
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2018-12-28 18:11:05 +0300

    Imported all models

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ import os
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 from app import create_app, db
+from app.models import Post, User, Tag, post_tag
 
 app = create_app(os.environ.get('FLASK_CONFIG') or 'default')
 
@@ -10,7 +11,14 @@ migrate = Migrate(app, db, compare_type=True)
 
 
 def make_shell_context():
-    return {'app': app, 'db': db}
+    return {
+        'app': app, 
+        'db': db,
+        'Post': Post,
+        'User': User,
+        'Tag': Tag,
+        'post_tag': post_tag,
+    }
 
 
 manager.add_command("db", MigrateCommand)
@@ -19,3 +27,4 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
     manager.run()
+    db.create_all()
